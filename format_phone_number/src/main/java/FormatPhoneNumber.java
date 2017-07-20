f741383ffe5f4c96857932b3efb82c122a48942e
Nitiwat Nasomvas <nitiwat.n@kasikornbank.com>
2017-07-20 14:17:53 +0700

    PhoneNum last case5 : add "-" to length10

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -1,37 +1,48 @@
 public class FormatPhoneNumber {
 
     public String removeNonDigit(String input) {
-        String x = input.replace("-","");
-        x = x.replace(" ","");
-        return x;
-
+        String Result = input.replace("-","");
+        Result = Result.replace(" ","");
+        return Result;
     }
 
     public String solution(String input) {
-        String x = "";
-        if(removeNonDigit(input).length()==9) {
-            for (int i = 0; i < input.length(); i++) {
-                if (i == 3) {
-                    x += "-" + input.charAt(i);
-                } else if (i == 6) {
-                    x += "-" + input.charAt(i);
+        String Result = "";
+        String Input = removeNonDigit(input);
+        if (Input.length() == 9) {
+            for (int Index = 0; Index < Input.length(); Index++) {
+                if (Index == 3) {
+                    Result += "-" + Input.charAt(Index);
+                } else if (Index == 6) {
+                    Result += "-" + Input.charAt(Index);
                 } else
-                    x += input.charAt(i);
+                    Result += Input.charAt(Index);
             }
-        } else
-        for (int i = 0; i < removeNonDigit(input).length(); i++) {
-            if (i == 3) {
-                x += "-" + removeNonDigit(input).charAt(i);
-            } else if (i == 6) {
-                x += "-" + removeNonDigit(input).charAt(i);
-            } else if (i == 9) {
-                x += "-" + removeNonDigit(input).charAt(i);
-            } else  if (i == 12) {
-                x += "-" + removeNonDigit(input).charAt(i);
-            } else
-                x += removeNonDigit(input).charAt(i);
+        } else if (Input.length() == 14) {
+            for (int Index = 0; Index < Input.length(); Index++) {
+                if (Index == 3) {
+                    Result += "-" + Input.charAt(Index);
+                } else if (Index == 6) {
+                    Result += "-" + Input.charAt(Index);
+                } else if (Index == 9) {
+                    Result += "-" + Input.charAt(Index);
+                } else if (Index == 12) {
+                    Result += "-" + Input.charAt(Index);
+                } else
+                    Result += Input.charAt(Index);
+            }
+        } else {
+            for (int Index = 0; Index < Input.length(); Index++) {
+                if (Index == 3) {
+                    Result += "-" + Input.charAt(Index);
+                } else if (Index == 6) {
+                    Result += "-" + Input.charAt(Index);
+                } else if (Index == 8) {
+                    Result += "-" + Input.charAt(Index);
+                } else
+                    Result += Input.charAt(Index);
+            }
         }
-        return x;
+        return Result;
     }
-
 }
