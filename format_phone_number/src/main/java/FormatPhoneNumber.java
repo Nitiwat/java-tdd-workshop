a5b9bb00433b5571d55b663aeeae445078c26762
Nitiwat Nasomvas <nitiwat.n@kasikornbank.com>
2017-07-20 13:52:59 +0700

    PhoneNum case3 : remove "-" and " "

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -1,14 +1,23 @@
 public class FormatPhoneNumber {
 
     public String removeNonDigit(String input) {
-         String x = input.replace("-","");
-         x = x.replace(" ","");
-         return x;
+        String x = input.replace("-","");
+        x = x.replace(" ","");
+        return x;
 
     }
 
     public String solution(String input) {
-        return null;
+        String x = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (i == 3) {
+                x += "-" + input.charAt(i);
+            } else if (i == 6) {
+                x += "-" + input.charAt(i);
+            } else
+                x += input.charAt(i);
+        }
+        return x;
     }
 
 }
