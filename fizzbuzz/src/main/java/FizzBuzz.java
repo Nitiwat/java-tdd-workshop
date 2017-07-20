0e75d045945bff2b53006126727091e61cd40251
Nitiwat Nasomvas <nitiwat.n@kasikornbank.com>
2017-07-20 09:48:04 +0700

    Test FizzBuzz Input(1 to 30)

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,12 +1,23 @@
 public class FizzBuzz {
-    public String say(int input) {
+    public static String say(int input) {
          if(input%15==0){
              return "FizzBuzz";
-         } else if (input%5==0){
-             return "Buzz";
          } else if (input%3==0){
              return "Fizz";
+         } else if (input%5==0){
+             return "Buzz";
          }
         return String.valueOf(input);
     }
+
+    public static void main(String[] args) {
+        for(int runtime=1;runtime<=30;runtime++){
+            System.out.println("Input ="+runtime+"\t=> output = "+say(runtime));
+        }
+
+    }
+
 }
+
+
+
