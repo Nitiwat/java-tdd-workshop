9c441574e71f70117bc407e8e5748ac8b7462092
Nitiwat Nasomvas <nitiwat.n@kasikornbank.com>
2017-07-20 14:04:27 +0700

    PhoneNum case4 : add "-" to length9 and length14

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -9,13 +9,27 @@ public class FormatPhoneNumber {
 
     public String solution(String input) {
         String x = "";
-        for (int i = 0; i < input.length(); i++) {
+        if(removeNonDigit(input).length()==9) {
+            for (int i = 0; i < input.length(); i++) {
+                if (i == 3) {
+                    x += "-" + input.charAt(i);
+                } else if (i == 6) {
+                    x += "-" + input.charAt(i);
+                } else
+                    x += input.charAt(i);
+            }
+        } else
+        for (int i = 0; i < removeNonDigit(input).length(); i++) {
             if (i == 3) {
-                x += "-" + input.charAt(i);
+                x += "-" + removeNonDigit(input).charAt(i);
             } else if (i == 6) {
-                x += "-" + input.charAt(i);
+                x += "-" + removeNonDigit(input).charAt(i);
+            } else if (i == 9) {
+                x += "-" + removeNonDigit(input).charAt(i);
+            } else  if (i == 12) {
+                x += "-" + removeNonDigit(input).charAt(i);
             } else
-                x += input.charAt(i);
+                x += removeNonDigit(input).charAt(i);
         }
         return x;
     }
