427ca8abc03f3a2d191ca27042947a89a62087c1
Nitiwat Nasomvas <nitiwat.n@kasikornbank.com>
2017-07-20 09:38:17 +0700

    แก้ case9 input20 => return "Buzz"

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,12 +1,12 @@
 public class FizzBuzz {
     public String say(int input) {
-         if(input==3 || input==6 || input==9) {
-            return "Fizz";
-        } else if (input==5 || input==10) {
-            return "Buzz";
-        } else if (input ==15){
+         if(input==15){
              return "FizzBuzz";
-         } else
+         } else if (input%5==0){
+             return "Buzz";
+         } else if (input%3==0){
+             return "Fizz";
+         }
         return String.valueOf(input);
     }
 }
